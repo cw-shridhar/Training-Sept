d0ae6099317ec37505dc541a1418c9c27de771d8
Shridhar Shriganesh Kalukhe <shridhar@aepl.com>
2022-09-28 13:46:06 +0530

    learning C#

---
+++ Day 10/Tutorial/Tutorial/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // #1
+            //Console.WriteLine("Hello C++");
+            //Console.ReadLine();
+
+            // #2
+            /*
+            Integer - int a; 4butes
+            long - 8 bytes
+            double - 8 bytes
+            float b; 4bytes
+            char c; 2bytes
+            bool isGreat = true; 1bit
+            string s = "shridhar"; 2 bytes per character
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+             */
+
+            // #3
+            /*
+             int a = 4;
+            // float b = 24.4; // this will give error, since C# is type safe language
+            float b = 24.4F; // this will give error
+            double c = 12.5;
+            // by default decimal number is in double
+             */
+
+            // #4
+            /*
+             There are 2 types of casting
+            1. implicit casting
+            char to int to long to float to double (C# does it automatically)
+            2. explicit casting
+             we have to do it manually
+            int a = (int)4.5;
+            float b = Convert.ToInt32(3.55);
+            Console.WriteLine(a);
+             */
+
+            // #5 taking input
+            Console.WriteLine("enter a number : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter a number : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a + b);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day 11/LinqPlaylist/LinqPlaylist/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPlaylist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("hello linq");
+            //Console.ReadLine();
+
+            // #1 Class Extention Method
+                //string s = "shridhar";
+            /******** 
+             * This is normal method without extention
+             * Just create class and add that method
+                string res = StringHelper.ChangeFirstLetterCase(s);
+            Console.WriteLine(res);
+             */
+
+            /*
+             * This is using Extention
+            string res = s.ChangeFirstLetterCase();
+            Console.WriteLine(res);
+             */
+
+            // without linq - smallest even number
+            //int[] Numbers = {1,2, 3, 4, 5 };
+            //int? res = null;
+            //foreach(int i in Numbers)
+            //{
+            //    if(i%2 == 0)
+            //    {
+            //        if (res == null || i < res)
+            //        {
+            //            res = i;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine(res);
+
+            // with linq
+            //int result = Numbers.Where(x => x%2 == 0).Min();
+            //Console.WriteLine(result);
+
+            // without linq - country with smallest length;
+            //string[] countries = { "India", "USA", "UK" };
+            //int res = 100000;
+
+            //foreach(string s in countries)
+            //{
+            //    if(s.Length < res)
+            //    {
+            //        res = s.Length;
+            //    }
+            //}
+
+            //Console.WriteLine(res);
+
+
+            //with 
+            //Console.WriteLine(countries.Min(country => country.Length));
+
+
+        }
+    }
+}
