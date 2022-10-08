b2e7cf6e58ff49466dbbe8f88efb1efedec80eaf
Shridhar Shriganesh Kalukhe <shridhar@aepl.com>
2022-10-08 19:07:24 +0530

    dependency injection

---
+++ Day 12/DependencyInjectionTutorial/DependencyInjectionTutorial/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionTutorial
+{
+    public interface IAccount
+    {
+        void PrintDetails();
+    }
+    class CurrentAccount : IAccount
+    {
+        public void PrintDetails()
+        {
+            Console.WriteLine("Details of Current Account");
+        }
+    }
+
+    class SavingAccount : IAccount
+    {
+        public void PrintDetails()
+        {
+            Console.WriteLine("Details of Saving Account");
+        }
+    }
+
+    class Account
+    {
+        private IAccount account;
+        public Account(IAccount account) // parametarized constructor
+        {
+            this.account = account;
+        }
+
+        public void PrintAccounts()
+        {
+            account.PrintDetails(); 
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IAccount sa = new SavingAccount();
+            IAccount ca = new CurrentAccount();
+
+            Account a = new Account(ca);
+            Account b = new Account(sa);
+            a.PrintAccounts();
+            b.PrintAccounts();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day 12/DependencyInjectionTutorial/DependencyInjectionTutorial/TightCouple.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionTutorial
+{
+    /*
+    class CurrentAccount
+    {
+        public void PrintDetails()
+        {
+            Console.WriteLine("Details of Current Account");
+        }
+    }
+
+    class SavingAccount
+    {
+        public void PrintDetails()
+        {
+            Console.WriteLine("Details of Saving Account");
+        }
+    }
+
+    class Account
+    {
+        CurrentAccount ca = new CurrentAccount();
+        SavingAccount sa = new SavingAccount();
+
+        public void PrintAccounts()
+        {
+            ca.PrintDetails();
+            sa.PrintDetails();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a = new Account();
+            a.PrintAccounts();
+
+            //Console.ReadLine();
+        }
+    }
+    */
+}
